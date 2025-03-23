5df8ddf07b71cdb7b025730691e51e4b3dedfa4c
Wallan <wallanaq.dev@gmail.com>
2025-03-23 20:05:46 -0300

    initial commit

---
+++ src/main/java/com/example/SpringBootKafkaLabApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaLabApplication.class, args);
+	}
+
+}
